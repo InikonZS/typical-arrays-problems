b17f373b92ef06bf054b4a0203df1c27ba7b1488
Inikon <ZhanZS@mail.ru>
2020-02-20 03:31:06 +0300

    fix: solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
+function check(array){
+  if (!array||!array.length) {return false;}
+  return true;
+}
 exports.min = function min (array) {
-  return 0;
+  if (!check(array)) return 0;
+  //if (!array||!array.length) {return 0;}
+  //return Math.min(...array);
+  return array.reduce((p,c)=>p<c?p:c);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!check(array)) return 0;
+  //if (!array||!array.length) {return 0;}
+  //return Math.max(...array);
+  return array.reduce((p,c)=>p<c?c:p);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (check(array)) ? array.reduce((p,c)=>p+c)/array.length : 0;
 }
